0a7765c6922f6fd137fb20bd28d5122bad2b87b5
Panuwat JR <poompanuwat66@gmail.com>
2025-07-29 10:04:14 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart'; // Import ไลบรารี Material ของ Flutter
+
+void main() {
+  // จุดเริ่มต้นของแอป Flutter
+  runApp(const MyApp()); // บอก Flutter ว่าวิดเจ็ตใดคือรากฐานของแอป
+}
+
+// MyApp โดยทั่วไปคือวิดเจ็ตรากฐานของแอปพลิเคชันของคุณ
+class MyApp extends StatelessWidget {
+  const MyApp({super.key}); // Constructor
+
+  @override
+  Widget build(BuildContext context) {
+    // เมธอด build() ส่งคืนโครงสร้างต้นไม้วิดเจ็ตสำหรับวิดเจ็ตนี้
+    return MaterialApp(
+      title: 'My Awesome App',
+      debugShowCheckedModeBanner: false, // ซ่อนแบนเนอร์ debug
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true, // ใช้ Material Design เวอร์ชันล่าสุด
+      ),
+      home: const MyHomePage(), // วิดเจ็ตหน้าจอหลักของคุณ
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // ให้โครงสร้างแอปพื้นฐาน
+      appBar: AppBar(
+        title: const Text('Home Page'),
+        backgroundColor: Colors.blue, // ปรับแต่งสี AppBar
+      ),
+      body: Center(
+        // พื้นที่เนื้อหาหลัก
+        child: Container(
+          padding: EdgeInsets.all(10),
+          width: 200.0,
+          height: 100.0,
+          decoration: BoxDecoration(
+            color: Colors.red,
+            borderRadius: BorderRadius.horizontal(
+              left: Radius.circular(15.0),
+              right: Radius.elliptical(50, 50),
+            ),
+          ),
+          child: Center(
+            child:Image.asset(
+              'assets/images/messi-1805.jpg',
+              width: 150,
+              // height: 100, // สามารถละเว้นมิติหนึ่งเพื่อรักษาอัตราส่วนภาพ
+              fit: BoxFit.contain, // ปรับขนาดให้พอดีภายในกล่องโดยไม่ตัดภาพ
+            ),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // การกระทำที่จะทำเมื่อกดปุ่ม
+          print('FAB Pressed!');
+        },
+        child: const Icon(Icons.add), // ไอคอนสำหรับปุ่ม
+      ),
+    );
+  }
+}
